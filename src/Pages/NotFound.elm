ea8e588d167660661d31e120622152ec649eeb52
ChristianVisintin <christian.visintin1997@gmail.com>
2021-02-21 09:36:46 +0100

    404 page centered

--- src/Pages/NotFound.elm
+++ src/Pages/NotFound.elm
@@ -10,7 +10,7 @@ module Pages.NotFound exposing (view)
 import Css exposing (..)
 import Html
 import Html.Styled exposing (..)
-import Html.Styled.Attributes exposing (class, href)
+import Html.Styled.Attributes exposing (class, css, href)
 
 
 
@@ -20,9 +20,9 @@ import Html.Styled.Attributes exposing (class, href)
 view : Html msg
 view =
     div [ class "container-fluid" ]
-        [ div [ class "row" ]
+        [ div [ class "row", class "align-content-center", class "justify-content-center" ]
             [ div [ class "col-12", class "align-content-center", class "justify-content-center" ]
-                [ h1 [] [ text "The page you were looking for, doesn't exist!" ]
+                [ h1 [ css [ textAlign center ] ] [ text "The page you were looking for, doesn't exist!" ]
                 , a [ href "/" ] [ text "Go back to home" ]
                 ]
             ]
